b2e8fe863b07d5577a574bbbc218c6b36f5b9d8d
Brandon <brandon.shepard6@icloud.com>
2020-11-23 21:33:22 -0600

    4-12 renamed to itoa

--- chapter4/exercise4-12.c
+++ chapter4/exercise4-12.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-int printd(int n, char s[], int i);
+int itoa(int n, char s[], int i);
 
 int main(){
 	char s[10];
 	int n = -2431;
-	printd(n,s,0);
+	itoa(n,s,0);
 	printf("s: %s\n", s);
 	return 0;
 }
 
-int printd(int n, char s[], int i){
+int itoa(int n, char s[], int i){
 	if(n < 0){
 		s[i++] = '-';
 		n *= -1;
 	}
 	if(n / 10){
-		i = printd(n/10, s, i);
+		i = itoa(n/10, s, i);
 	}
 	s[i++] = (n % 10) + '0';
 	s[i] = '\0';
